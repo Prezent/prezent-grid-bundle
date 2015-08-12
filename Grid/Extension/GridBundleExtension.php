5811482bfae93dd64e5680bd507781864fb241e0
Sander Marechal <sander@prezent.nl>
2015-08-12 13:44:14 +0200

    Configure route extension

--- Grid/Extension/GridBundleExtension.php
+++ Grid/Extension/GridBundleExtension.php
@@ -3,7 +3,9 @@
 namespace Prezent\GridBundle\Grid\Extension;
 
 use Prezent\Grid\BaseGridExtension;
+use Prezent\Grid\VariableResolver;
 use Prezent\GridBundle\Grid\Type;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 /**
  * Symfony type extensions
@@ -14,11 +16,34 @@ use Prezent\GridBundle\Grid\Type;
 class GridBundleExtension extends BaseGridExtension
 {
     /**
+     * @var UrlGeneratorInterface
+     */
+    private $router;
+
+    /**
+     * @var VariableResolver
+     */
+    private $resolver;
+
+    /**
+     * Constructor
+     *
+     * @param UrlGeneratorInterface $router
+     * @param VariableResolver $resolver
+     */
+    public function __construct(UrlGeneratorInterface $router, VariableResolver $resolver)
+    {
+        $this->router = $router;
+        $this->resolver = $resolver;
+    }
+
+    /**
      * {@inheritDoc}
      */
     protected function loadColumnTypeExtensions()
     {
         return [
+            new Type\RouteTypeExtension($this->router, $this->resolver),
             new Type\TranslatableLabelTypeExtension(),
         ];
     }
