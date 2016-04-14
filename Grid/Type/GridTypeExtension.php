ff4ff0d448f5d0b917223f15fdd1cef4138a789d
Sander Marechal <sander@prezent.nl>
2016-04-14 12:42:16 +0200

    Inherit label_translation_domain from grid

---
+++ Grid/Type/GridTypeExtension.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Prezent\GridBundle\Grid\Type;
+
+use Prezent\Grid\BaseGridTypeExtension;
+use Prezent\Grid\Extension\Core\GridType;
+
+/**
+ * GridTypeExtension
+ *
+ * @see BaseGridTypeExtension
+ * @author Sander Marechal
+ */
+class GridTypeExtension extends BaseGridTypeExtension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'label_translation_domain' => 'messages',
+        ]);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function buildView(ElementView $view, array $options)
+    {
+        $view->vars['label_translation_domain'] = $options['label_translation_domain'];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getExtendedType()
+    {
+        return GridType::class;
+    }
+}
--- Grid/Type/TranslatableLabelTypeExtension.php
+++ Grid/Type/TranslatableLabelTypeExtension.php
@@ -21,7 +21,7 @@ class TranslatableLabelTypeExtension extends BaseElementTypeExtension
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'label_translation_domain' => 'messages',
+            'label_translation_domain' => null,
         ]);
     }
 
@@ -30,7 +30,7 @@ class TranslatableLabelTypeExtension extends BaseElementTypeExtension
      */
     public function buildView(ElementView $view, array $options)
     {
-        $view->vars['label_translation_domain'] = $options['label_translation_domain'];
+        $view->vars['label_translation_domain'] = $options['label_translation_domain'] ?: $view->parent->vars['label_translation_domain'];
     }
 
     /**
