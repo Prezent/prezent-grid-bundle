95cdeabdc21a8617d78e79c66622801e3fd28101
Sander Marechal <sander@prezent.nl>
2017-10-16 17:21:38 +0200

    Allow auto-configuration of grid services

--- DependencyInjection/PrezentGridExtension.php
+++ DependencyInjection/PrezentGridExtension.php
@@ -2,6 +2,7 @@
 
 namespace Prezent\GridBundle\DependencyInjection;
 
+use Prezent\Grid;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
@@ -25,6 +26,23 @@ class PrezentGridExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+
+        if (method_exists($container, 'registerForAutoconfiguration')) {
+            $container->registerForAutoconfiguration(Grid\GridType::class)
+                ->addTag('prezent_grid.grid');
+            $container->registerForAutoconfiguration(Grid\GridTypeExtension::class)
+                ->addTag('prezent_grid.grid_type_extension');
+            $container->registerForAutoconfiguration(Grid\ElementType::class)
+                ->addTag('prezent_grid.element_type');
+            $container->registerForAutoconfiguration(Grid\ElementTypeExtension::class)
+                ->addTag('prezent_grid.element_type_extension');
+            $container->registerForAutoconfiguration(Grid\GridExtension::class)
+                ->addTag('prezent_grid.grid_extension');
+            $container->registerForAutoconfiguration(Grid\ElementTypeFactory::class)
+                ->addTag('prezent_grid.element_type_factory');
+            $container->registerForAutoconfiguration(Grid\VariableResolver::class)
+                ->addTag('prezent_grid.variable_resolver');
+        }
 
         if ($config['themes']) {
             $container->setParameter('prezent_grid.twig_renderer.themes', $config['themes']);
